9115b99bf2e6d54cd3b6200773665af46d415dfd
hanjoon-kim <hanjoon3@gmail.com>
2018-07-10 10:57:33 -0700

    Implement working connection to server

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,18 @@
+var express = require('express');
+var app = express();
+var port = 3000;
 
+app.use(express.static('client'));
+app.get('client/index.html', function(req, res) {
+  res.sendFile(__dirname + "/" + 'index.html');
+})
 
+app.get('/process_get', function(req, res) {
+  response = {
+    CSV: req.query.input
+  };
+  console.log(response);
+  res.end(JSON.stringify(response));
+})
+
+app.listen(port, () => console.log(`listening on ${port}`));
