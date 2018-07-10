e0af625aeab68b621f324449c7b2c3e224a9ba3d
hanjoon-kim <hanjoon3@gmail.com>
2018-07-10 12:26:16 -0700

    Implement functional server and client that returns input exactly

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,17 @@
 console.log('hello');
 
+$('#form').on('submit', function(e){
+  e.preventDefault();
+  
+  var input = $("input").val();
+  console.log('form submitted')
+  console.log(input);
+
+  $.post('/', function(err, data) {
+    console.log(data);
+    $('#results span').text(input);
+  });
+
+});
+
+//action="http://127.0.0.1:3000 method="POST"
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,18 +1,30 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 var port = 3000;
+
+var urlencodedParser = bodyParser.urlencoded({extended: false});
 
 app.use(express.static('client'));
 app.get('client/index.html', function(req, res) {
   res.sendFile(__dirname + "/" + 'index.html');
 })
 
-app.get('/process_get', function(req, res) {
+app.post('/', urlencodedParser, function(req, res) {
+  console.log(req);
   response = {
-    CSV: req.query.input
+    CSV: req.body.input
   };
-  console.log(response);
   res.end(JSON.stringify(response));
 })
+
+
+// app.get('/process_get', function(req, res) {
+//   response = {
+//     CSV: req.query.input
+//   };
+//   console.log(response);
+//   res.end(JSON.stringify(response));
+// })
 
 app.listen(port, () => console.log(`listening on ${port}`));
