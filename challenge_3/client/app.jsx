d5a36db73bffd6034a8b049dc5502e0ac6653e4b
hanjoon-kim <hanjoon3@gmail.com>
2018-07-11 12:24:52 -0700

    Implement checkout button

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,28 @@
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { checkout: false };
+  }
+  
+  render() {
+    if (this.state.checkout) {
+      console.log('checkedout');
+    }
+    
+    return ( 
+      <button onClick={() => this.setState({checkout: true})} >Checkout</button>
+
+    );
+
+// 'button', {
+      // onClick: () => this.setState({ checkout: true }),
+    // }, 'Checkout');
+  }
+}
 
 
+
+
+
+const domContainer = document.getElementById('app');
+ReactDOM.render(<App/>, domContainer);
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,7 @@
+const express = require('express');
+const app = express();
+var port = 3000;
 
+app.listen(port, () => console.log(`Listening on port ${port}!`));
 
+app.use(express.static('public'));
