a54ccc6c078bbb3343b51a5e01791c002bdd7b51
hanjoon-kim <hanjoon3@gmail.com>
2018-07-11 16:06:16 -0700

    Implement cycling front-end web

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,7 +1,10 @@
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { checkout: false };
+    this.state = { 
+      checkout: false,
+      form: 1
+    };
   }
   
   render() {
@@ -9,19 +12,133 @@ class App extends React.Component {
       console.log('checkedout');
     }
     
-    return ( 
-      <button onClick={() => this.setState({checkout: true})} >Checkout</button>
+    if (this.state.form === 1) {
+      return ( 
+        <button onClick={() => this.setState({form: 2})}>Checkout</button>
+      );
+    }
 
-    );
-
-// 'button', {
-      // onClick: () => this.setState({ checkout: true }),
-    // }, 'Checkout');
+    if (this.state.form === 2) {
+      return (
+        <AccountFields />
+      )
+    }
   }
 }
 
+var fieldValues = {
+  name: null,
+  email: null,
+  password: null,
+  addressOne: null,
+  addressTwo: null,
+  city: null,
+  state: null,
+  zipCode: null,
+  phoneNumber: null,
+  creditCard: null,
+  expireDate: null,
+  cvv: null,
+  billingZip: null
+}
 
+class AccountFields extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      form: 2
+    };
+  }
+  render() {
 
+    if (this.state.form === 3) {
+      return (
+        <AddressFields />
+      )
+    }
+    return(
+      <div>
+        <form>
+          <label>Name:</label> 
+            <input type="text" name="name"/><br/>
+          <label>Email:</label> 
+            <input type="text" name="email"/><br/>
+          <label>Password:</label> 
+            <input type="text" name="password"/><br/>
+        </form>
+        <button onClick={() => this.setState({form: 3})}>Next</button>
+      </div>
+    )
+  }
+}
+
+class AddressFields extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      form: 3
+    };
+  }
+  render() {
+
+    if (this.state.form === 4) {
+      return (
+        <PaymentFields />
+      )
+    }
+
+    return(
+      <div>
+        <form>
+          <label>Address Line 1: </label> 
+            <input type="text" name="addressOne"/><br/>
+          <label>Address Line 2: </label> 
+            <input type="text" name="addressTwo"/><br/>
+          <label>City: </label> 
+            <input type="text" name="city"/><br/>
+          <label>State: </label> 
+            <input type="text" name="state"/><br/>
+          <label>Zip Code: </label> 
+            <input type="text" name="zipCode"/><br/>
+        </form>
+        <button onClick={() => this.setState({form: 4})}>Next</button>
+      </div>
+    )
+  }
+}
+
+class PaymentFields extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      form: 4
+    };
+  }
+  render() {
+
+    if (this.state.form === 1) {
+      return (
+        <App />
+      )
+    }
+
+    return(
+      <div>
+        <form>
+          <label>Credit Card Number: </label> 
+            <input type="text" name="creditCard"/><br/>
+          <label>Expiry Date: </label> 
+            <input type="text" name="expireDate"/><br/>
+          <label>CVV: </label> 
+            <input type="text" name="cvv"/><br/>
+          <label>Billing Zip Code: </label> 
+            <input type="text" name="billingZip"/><br/>
+        </form>
+        <button onClick={() => this.setState({form: 1})}>Purchase!</button>
+      </div>
+    )
+  }
+}
 
 
 const domContainer = document.getElementById('app');
