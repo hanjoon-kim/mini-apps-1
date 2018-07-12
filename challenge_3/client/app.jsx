817abfe538662e94a22b78e69030057776d51cc5
hanjoon-kim <hanjoon3@gmail.com>
2018-07-11 17:33:06 -0700

    Implement front-end with registering inputs

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -46,9 +46,29 @@ class AccountFields extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      form: 2
+      form: 2,
+      name: '',
+      email: '',
+      password:''
     };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
+  
+  handleChange(e) {
+    const value = e.target.value;
+    const name = e.target.name;
+
+    this.setState({
+      [name]: value
+    })
+  }
+  
+  handleSubmit(e) {
+    console.log('submitted woo' + this.state.name + this.state.email + this.state.password);
+    e.preventDefault();
+  }
+  
   render() {
 
     if (this.state.form === 3) {
@@ -60,13 +80,13 @@ class AccountFields extends React.Component {
       <div>
         <form>
           <label>Name:</label> 
-            <input type="text" name="name"/><br/>
+            <input type="text" name="name" onChange={this.handleChange}/><br/>
           <label>Email:</label> 
-            <input type="text" name="email"/><br/>
+            <input type="text" name="email" onChange={this.handleChange}/><br/>
           <label>Password:</label> 
-            <input type="text" name="password"/><br/>
+            <input type="password" name="password" onChange={this.handleChange}/><br/>
+            <button type="button" onClick={(e) => {this.handleSubmit(e); this.setState({form: 3});}}>Next</button>
         </form>
-        <button onClick={() => this.setState({form: 3})}>Next</button>
       </div>
     )
   }
@@ -76,9 +96,32 @@ class AddressFields extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      form: 3
+      form: 3,
+      addressOne: '',
+      addressTwo: '',
+      city: '',
+      state: '',
+      zipCode: ''
+      
     };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
+
+   handleChange(e) {
+    const value = e.target.value;
+    const name = e.target.name;
+
+    this.setState({
+      [name]: value
+    })
+  }
+  
+  handleSubmit(e) {
+    console.log('submitted woo ' + this.state.zipCode + this.state.addressTwo + this.state.state);
+    e.preventDefault();
+  }
+
   render() {
 
     if (this.state.form === 4) {
@@ -91,17 +134,17 @@ class AddressFields extends React.Component {
       <div>
         <form>
           <label>Address Line 1: </label> 
-            <input type="text" name="addressOne"/><br/>
+            <input type="text" name="addressOne" onChange={this.handleChange}/><br/>
           <label>Address Line 2: </label> 
-            <input type="text" name="addressTwo"/><br/>
+            <input type="text" name="addressTwo" onChange={this.handleChange}/><br/>
           <label>City: </label> 
-            <input type="text" name="city"/><br/>
+            <input type="text" name="city" onChange={this.handleChange}/><br/>
           <label>State: </label> 
-            <input type="text" name="state"/><br/>
+            <input type="text" name="state" onChange={this.handleChange}/><br/>
           <label>Zip Code: </label> 
-            <input type="text" name="zipCode"/><br/>
+            <input type="text" name="zipCode" onChange={this.handleChange}/><br/>
         </form>
-        <button onClick={() => this.setState({form: 4})}>Next</button>
+        <button onClick={(e) => {this.handleSubmit(e); this.setState({form: 4});}}>Next</button>
       </div>
     )
   }
@@ -111,14 +154,35 @@ class PaymentFields extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      form: 4
+      form: 4,
+      creditCard: '',
+      expireDate: '',
+      cvv: '',
+      billingZip: ''
     };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
+
+   handleChange(e) {
+    const value = e.target.value;
+    const name = e.target.name;
+
+    this.setState({
+      [name]: value
+    })
+  }
+  
+  handleSubmit(e) {
+    console.log('submitted woo' + this.state.creditCard + this.state.expireDate + this.state.cvv);
+    e.preventDefault();
+  }
+  
   render() {
 
-    if (this.state.form === 1) {
+    if (this.state.form === 5) {
       return (
-        <App />
+        <Confirmation />
       )
     }
 
@@ -126,14 +190,42 @@ class PaymentFields extends React.Component {
       <div>
         <form>
           <label>Credit Card Number: </label> 
-            <input type="text" name="creditCard"/><br/>
+            <input type="text" name="creditCard" onChange={this.handleChange}/><br/>
           <label>Expiry Date: </label> 
-            <input type="text" name="expireDate"/><br/>
+            <input type="text" name="expireDate" onChange={this.handleChange}/><br/>
           <label>CVV: </label> 
-            <input type="text" name="cvv"/><br/>
+            <input type="text" name="cvv" onChange={this.handleChange}/><br/>
           <label>Billing Zip Code: </label> 
-            <input type="text" name="billingZip"/><br/>
+            <input type="text" name="billingZip" onChange={this.handleChange}/><br/>
         </form>
+        <button onClick={(e) => {this.handleSubmit(e); this.setState({form: 5});}}>Next</button>
+      </div>
+    )
+  }
+}
+
+class Confirmation extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      form: 5
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  
+  handleSubmit(e) {
+    console.log('submitted woo' + this.state.name + this.state.email + this.state.password);
+    e.preventDefault();
+  }
+  
+  render() {
+    
+    if (this.state.form === 1) {
+      return (<App />);
+    }
+
+    return(
+      <div>
         <button onClick={() => this.setState({form: 1})}>Purchase!</button>
       </div>
     )
